d88a7d73789571757197cc01949953d072940f4d
Liam <77995100+BirchBoy@users.noreply.github.com>
2021-08-03 15:07:02 -0400

    Add files via upload
    Complete rework of the bot.

--- Bot.py
+++ Bot.py
@@ -1,368 +1,253 @@
-import os
 import random
-from twitchio.ext import commands
-from rcon import Client
 from time import sleep
 
-chat_input = ""
+from twitchio.ext import commands
+from rcon import Client
 
 # Settings for how long a command will last.
+
+# Account stuff
+username = "borchboy"
+tw_token = 'y3lxl3uxpghaqq2u2yrgy4cs96ipss'
+# The prefix prefix can be anything. here are some examples: ! @ # $ % % ^ & * < > ?
+tw_prefix = "#"
+
+# Networking
+tf_ip = "127.0.0.1"
+tf_port = 55635
+tf_password = "FunnyPasswordForNerds"
+
+# Timing for the commands
 movement_time = int(2)
-attack_time = int(1)
-look_time = int(2)
+attack_time = int(3)
+look_time = int(1)
+medic_call_time = int(30)
 # 0-1 not yet added.
 taunt_active = int(0)
 random_class = int(0)
 
 
-# Random class logic. Not yet added.
-def ran_class():
-    if random_class == 1:
-        rand_num = random.randint(1, 9)
-    return rand_num
-
-
-# Commands for twitch chat
-def attack():
+def attackC():
     global chat_input, client, attack_time
+    chat_input = client.run("say_party \"Used Attack1")
     chat_input = client.run("+attack")
     sleep(attack_time)
     chat_input = client.run("-attack")
     return chat_input
 
 
-def alt():
+def attack2C():
     global chat_input, client, attack_time
+    chat_input = client.run("say_party \"Used Attack2")
     chat_input = client.run("+attack2")
     sleep(attack_time)
     chat_input = client.run("-attack2")
     return chat_input
 
 
-def right():
+def rightC():
     global chat_input, client, look_time
+    chat_input = client.run("say_party \"Used Right")
     chat_input = client.run("+right")
     sleep(look_time)
     chat_input = client.run("-right")
     return chat_input
 
 
-def left():
+def leftC():
     global chat_input, client, look_time
+    client.run("say_party \"Used Left")
     chat_input = client.run("+left")
     sleep(look_time)
     chat_input = client.run("-left")
     return chat_input
 
 
-def forward():
+def forwardC():
     global chat_input, client, movement_time
+    client.run("say_party \"Used Forward")
     chat_input = client.run("+forward")
     sleep(movement_time)
     chat_input = client.run("-forward")
     return chat_input
 
 
-def back():
+def backC():
     global chat_input, client, movement_time
+    client.run("say_party \"Used Backwards")
     chat_input = client.run("+back")
     sleep(movement_time)
     chat_input = client.run("-back")
     return chat_input
 
 
-def jump():
+def jumpC():
     global chat_input, client
+    client.run("say_party \"Used Jump")
     chat_input = client.run("+jump")
     sleep(1)
     chat_input = client.run("-jump")
     return chat_input
 
 
-def use():
+def useC():
     global chat_input, client
+    client.run("say_party \"Used Use")
     chat_input = client.run("+use_action_slot_item")
     sleep(1)
     chat_input = client.run("-use_action_slot_item")
     return chat_input
 
 
-def rand_loadout():
+def rand_loadoutC():
     rand_load = random.randint(1, 4)
+    client.run("say_party \"Used Random Loadout {}".format(rand_load))
     chat_input = client.run("load_itempreset {}".format(rand_load))
     return chat_input
 
 
-def taunt():
-    rand_load = random.randint(1, 4)
+def tauntC():
+    client.run("say_party \"Used Taunt")
     chat_input = client.run("taunt")
     return chat_input
 
 
-def help():
+def helpC():
     global chat_input, client
-    count = 30
+    client.run("say_party \"Used HELP!")
+    count = medic_call_time
     while count > 0:
         count = count - 1
         chat_input = client.run("voicemenu 0 0")
+        sleep(2)
     return chat_input
 
 
-def scout():
+def scoutC():
     global chat_input, client
+    client.run("say_party \"Used Scout")
     chat_input = client.run("join_class scout")
     return chat_input
 
 
-def medic():
+def medicC():
     global chat_input, client
+    client.run("say_party \"Used Medic")
     chat_input = client.run("join_class medic")
     return chat_input
 
 
-def demoman():
+def demomanC():
     global chat_input, client
+    client.run("say_party \"Used Demoman")
     chat_input = client.run("join_class demoman")
     return chat_input
 
 
-def pyro():
+def pyroC():
     global chat_input, client
+    client.run("say_party \"Used Pyro")
     chat_input = client.run("join_class pyro")
     return chat_input
 
 
-def engineer():
+def engineerC():
     global chat_input, client
+    client.run("say_party \"Used Engineer Gaming")
     chat_input = client.run("join_class engineer")
     return chat_input
 
 
-def sniper():
+def sniperC():
     global chat_input, client
+    client.run("say_party \"Used Sniper")
     chat_input = client.run("join_class sniper")
     return chat_input
 
 
-def spy():
+def spyC():
     global chat_input, client
+    client.run("say_party \"Used Spy")
     chat_input = client.run("join_class spy")
     return chat_input
 
 
-def soldier():
+def soldierC():
     global chat_input, client
+    client.run("say_party \"Used Soldier")
     chat_input = client.run("join_class soldier")
     return chat_input
 
 
-def heavy():
+def heavyC():
     global chat_input, client
+    client.run("say_party \"Used Heavy")
     chat_input = client.run("join_class heavyweapons")
     return chat_input
 
 
-"""
-Dear dev,
-Be attracted to who you are attracted to.
-sincerely
-Birch
-"""
+class Bot(commands.Bot):
 
-# set up the bot
-bot = commands.Bot(
-    irc_token=os.environ['TMI_TOKEN'],
-    client_id=os.environ['CLIENT_ID'],
-    nick=os.environ['BOT_NICK'],
-    prefix=os.environ['BOT_PREFIX'],
-    initial_channels=[os.environ['CHANNEL']]
-)
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        super().__init__(token=tw_token, prefix=tw_prefix, initial_channels=[tw_username])
 
+    async def event_ready(self):
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
 
-# Twitch chat commands
-@bot.event
-async def event_ready():
-    'Called once when the bot goes online.'
-    print(f"{os.environ['BOT_NICK']} is online!")
-    ws = bot._ws  # this is only needed to send messages within event_ready
-    await ws.send_privmsg(os.environ["CHANNEL"], f"/me has landed!")
+    @commands.command()
+    async def stop(self, ctx: commands.Context):
+        await ctx.send("{0} Used stop program stopping.".format(ctx.author.name))
+        sleep(1)
+        await ctx.send("3")
+        sleep(1)
+        await ctx.send("2")
+        sleep(1)
+        await ctx.send("1")
+        sleep(1)
+        await ctx.send("Program terminated")
 
+    @commands.command()
+    async def help(self, ctx: commands.Context):
+        await ctx.send("{0} Used Help".format(ctx.author.name))
 
-@bot.event
-async def event_message(ctx):
-    """Runs every time a message is sent in chat."""
+    @commands.command()
+    async def attack1(self, ctx: commands.Context):
+        await ctx.send("{0} Used Attack1!".format(ctx.author.name))
+        attackC()
 
-    # make sure the bot ignores itself and the streamer
-    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
-        return
+    @commands.command()
+    async def attack2(self, ctx: commands.Context):
+        await ctx.send("{0} Used Attack2!".format(ctx.author.name))
+        attack2C()
 
-    await bot.handle_commands(ctx)
+    @commands.command()
+    async def right(self, ctx: commands.Context):
+        await ctx.send("{0} Used Right!".format(ctx.author.name))
+        rightC()
 
-    # await ctx.channel.send(ctx.content)
+    @commands.command()
+    async def left(self, ctx: commands.Context):
+        await ctx.send("{0} Used Left!".format(ctx.author.name))
+        leftC()
 
-    if 'hello' in ctx.content.lower():
-        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+    @commands.command()
+    async def forward(self, ctx: commands.Context):
+        await ctx.send("{0} Used Forward!".format(ctx.author.name))
+        forwardC()
 
+    @commands.command()
+    async def back(self, ctx: commands.Context):
+        await ctx.send("{0} Used Back!".format(ctx.author.name))
+        backC()
 
-@bot.command(name='attack')
-async def jumper(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        attack()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='alt')
-async def alt_attack(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        attack()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='right')
-async def righter(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        attack()
-    print(chat_input)
-    await ctx.send('E')
+    @commands.command()
+    async def jump(self, ctx: commands.Context):
+        await ctx.send("{0} Used Jump!".format(ctx.author.name))
+        jumpC()
 
 
-@bot.command(name='left')
-async def lefter(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        attack()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='forward')
-async def righter(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        forward()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='back')
-async def backer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        back()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='jump')
-async def jumper(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        jump()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='use')
-async def user(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        use()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='random loadout')
-async def rand_loader(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        rand_loadout()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='taunt')
-async def taunt(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        rand_loadout()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='!MEDIC')
-async def backer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        help()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='scout')
-async def scouter(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        scout()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='medic')
-async def medicer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        medic()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='demoman')
-async def demomaner(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        demoman()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='pyro')
-async def pyroer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        pyro()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='engineer')
-async def engineerer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        engineer()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='sniper')
-async def sniperer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        sniper()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='spy')
-async def spyer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        spy()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='soldier')
-async def soldierer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        soldier()
-    print(chat_input)
-    await ctx.send('E')
-
-
-@bot.command(name='heavy')
-async def heavyer(ctx):
-    with Client('127.0.0.1', 55636, passwd='FunnyPasswordForNerds') as client:
-        heavy()
-    print(chat_input)
-    await ctx.send('E')
-
-
-if __name__ == "__main__":
+with Client(tf_ip, tf_port, passwd=tf_password) as client:
+    bot = Bot()
     bot.run()
